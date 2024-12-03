8c1ad81894fdb91e6b24894075275c8047a394f9
Thiago Cardoso <thiagocpais@hotmail.com>
2024-12-03 19:17:12 -0300

    Adding spring framework build

---
+++ learningplataform/src/main/java/com/thiagopais/learningplataform/LearningplataformApplication.java
@@ -0,0 +1,13 @@
+package com.thiagopais.learningplataform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningplataformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningplataformApplication.class, args);
+	}
+
+}
---
+++ learningplataform/src/test/java/com/thiagopais/learningplataform/LearningplataformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagopais.learningplataform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningplataformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
